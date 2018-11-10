2283b0a42538b4b078b24aebefbff8caeb15616d
mirfmaster <mirfmaster@gmail.com>
2018-11-10 23:34:53 +0700

    PreparedInstall

---
+++ database/migrations/2018_11_07_125452_create_table_produk.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableProduk extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('idproduct');
+            $table->string('nameproduct',60);
+            $table->string('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
